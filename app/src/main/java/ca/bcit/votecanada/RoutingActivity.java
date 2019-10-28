5d50e6f3110888903c5e5482fc64e99f649013ca
A00992390 <wangkang970111@gmail.com>
2019-10-28 11:40:37 -0700

    initial commit

---
+++ app/src/main/java/ca/bcit/votecanada/RoutingActivity.java
@@ -0,0 +1,14 @@
+package ca.bcit.votecanada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoutingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routing);
+    }
+}
