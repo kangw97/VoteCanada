82211931bc8cf91dd78a55c550750ec33a4ed687
Jovan Sekhon <jovansekhon@hotmail.com>
2019-11-11 17:57:29 -0800

    Homepage

--- app/src/main/java/ca/bcit/votecanada/MainActivity.java
+++ app/src/main/java/ca/bcit/votecanada/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onPrepareClick(View V) {
-        Intent intent = new Intent(this, PrepareActivity.class);
+    public void onRoutingClick(View V) {
+        Intent intent = new Intent(this, RoutingActivity.class);
         startActivity(intent);
     }
 
-    public void onRoutingClick(View V) {
-        Intent intent = new Intent(this, RoutingActivity.class);
+    public void onPartyClick(View V) {
+        Intent intent = new Intent(this, PartyActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/ca/bcit/votecanada/PartyActivity.java
+++ app/src/main/java/ca/bcit/votecanada/PartyActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PrepareActivity extends AppCompatActivity {
+public class PartyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_prepare);
+        setContentView(R.layout.activity_party);
     }
 }
